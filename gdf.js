7584f405ba59b9e943eb1926f8748342360ed4c3
Mendelson Gusmão <mendelsongusmao@gmail.com>
2020-10-26 20:39:18 -0300

    first version of gdf

---
+++ gdf.js
@@ -0,0 +1,40 @@
+const crypto = require('crypto');
+const Pie = require('cli-pie');
+const df = require('@mendelgusmao/df');
+
+const md5 = (s) => crypto.createHash('md5').update(s).digest('hex');
+const hex2rgb = (hex) => [...Array(3).keys()].map(
+  (i) => parseInt(hex.substring(2 * i, 2 * i + 2), 16),
+);
+
+const main = async (args) => {
+  const objects = await df(['--total', ...args]);
+
+  const pieData = objects.map(
+    (object) => {
+      if (object.mountpoint === '-') {
+        return {
+          value: object.available,
+          label: 'free',
+          color: [255, 255, 255],
+        };
+      }
+
+      return {
+        value: object.used,
+        label: object.mountpoint,
+        color: hex2rgb(md5(object.mountpoint)),
+      };
+    },
+  );
+
+  const pie = new Pie(10, pieData, {
+    legend: true,
+  });
+
+  console.log(pie.toString());
+};
+
+(
+  async () => main(process.argv.slice(2))
+)();
